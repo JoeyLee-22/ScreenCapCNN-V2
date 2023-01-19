51ab6eca68e36e467d50b1d2ce9b98c0f0cf6db3
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2023-01-19 00:37:15 -0800

    version 1.0.1
    change one variable name to better reflect usage

--- productive_image_downloader.py
+++ productive_image_downloader.py
@@ -5,21 +5,21 @@ import time
 from progress.bar import IncrementalBar
 
 def download_rand_image(delete_images=False, num_to_download=0, image_width=500, image_height=500):
-    path_and_filename = "productive_images"
+    path_and_foldername = "productive_images"
     url = "https://picsum.photos/{}/{}".format(image_width, image_height) # website to generate random photos given size
     
     if delete_images:
         while True:
             user_input = input("\nCONFIRM PRODUCTIVE DATA DELETION (y/n): ")
             if user_input=='y':
-                shutil.rmtree(path_and_filename)
-                os.mkdir(path_and_filename)
+                shutil.rmtree(path_and_foldername)
+                os.mkdir(path_and_foldername)
                 break
             elif user_input=='n':
                 break
     
     if num_to_download>0:
-        num_images = len(os.listdir(path_and_filename))
+        num_images = len(os.listdir(path_and_foldername))
         print(),
         bar = IncrementalBar('DOWNLOADING RANDOM IMAGES:', max = num_to_download)
         start_time = time.time()
@@ -28,13 +28,13 @@ def download_rand_image(delete_images=False, num_to_download=0, image_width=500,
             response = requests.get(url)
             if response.status_code == 200:
                 file_name = 'productive_{}.jpg'.format(i+1+num_images)
-                file_path = path_and_filename + "/" + file_name
+                file_path = path_and_foldername + "/" + file_name
                 with open(file_path, 'wb') as f:
                     f.write(response.content)
         bar.finish()
         end_time = time.time() - start_time
         
-        print("TOTAL IMAGES: {}".format(len(os.listdir(path_and_filename))))
+        print("TOTAL IMAGES: {}".format(len(os.listdir(path_and_foldername))))
         print("DOWNLAOD TIME FOR {} IMAGES:".format(num_to_download), end = " ")
         if end_time > 3600:
             print('%dhr, %.1fmin\n' % (int(end_time/3600),((end_time-int(end_time/3600)*3600)/60)))
