6dfd70a40ea1eb78ada79cd62446dcebf28e3139
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2023-01-19 00:38:12 -0800

    version 1.0.1
    change one variable name to better reflect usage

--- unproductive_image_downloader.py
+++ unproductive_image_downloader.py
@@ -5,36 +5,36 @@ from PIL import Image
 from vidgear.gears import CamGear
 
 def download_youtube_frames(delete_frames=False, frames_needed=0, link="", frames_skipped=500):
-    stream = CamGear(source=link, stream_mode=True, time_delay=1).start()
-    path_and_filename = "unproductive_images"
+    # stream = CamGear(source=link, stream_mode=True, time_delay=1).start()
+    path_and_foldername = "unproductive_images"
     currentframe = 0
    
     if delete_frames:
         while True:
             user_input = input("\nCONFIRM UNPRODUCTIVE DATA DELETION (y/n): ")
             if user_input=='y':
-                shutil.rmtree(path_and_filename)
-                os.mkdir(path_and_filename)
+                shutil.rmtree(path_and_foldername)
+                os.mkdir(path_and_foldername)
                 break
             elif user_input=='n':
                 break
 
     if frames_needed>0:
-        while True:
-            for i in range(frames_skipped):
-                frame = stream.read() ### using functions from vidGear module
-            if frame is None:
-                break
+        # while True:
+        #     for i in range(frames_skipped):
+        #         frame = stream.read() ### using functions from vidGear module
+        #     if frame is None:
+        #         break
             
-            currentframe+=1
-            file_name = 'screenshot_{}.jpg'.format(currentframe)
-            file_path = path_and_filename + "/" + file_name
-            with open(file_path, 'wb') as f:
-                f.write(frame)
+        #     currentframe+=1
+        #     file_name = 'screenshot_{}.jpg'.format(currentframe)
+        #     file_path = path_and_foldername + "/" + file_name
+        #     with open(file_path, 'wb') as f:
+        #         f.write(frame)
                 
-            if currentframe==frames_needed:
-                break
+        #     if currentframe==frames_needed:
+        #         break
 
-        cv2.destroyAllWindows()
-        stream.stop()
+        # cv2.destroyAllWindows()
+        # stream.stop()
         print("\nFRAMES DOWNLOADED: {}\n".format(currentframe))
