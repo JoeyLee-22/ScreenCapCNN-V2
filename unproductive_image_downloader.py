66ebd72c6df713034149160397ee81029bddc72b
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2023-01-19 01:00:40 -0800

    version 2.0.0
    got rid of CamGear and used a downloaded mp4 instead to get frames from

--- unproductive_image_downloader.py
+++ unproductive_image_downloader.py
@@ -2,11 +2,11 @@ import cv2
 import os
 import shutil
 from PIL import Image
-from vidgear.gears import CamGear
 
 def download_youtube_frames(delete_frames=False, frames_needed=0, link="", frames_skipped=500):
-    # stream = CamGear(source=link, stream_mode=True, time_delay=1).start()
     path_and_foldername = "unproductive_images"
+    vidcap = cv2.VideoCapture('minecraft.mp4')
+    success,image = vidcap.read()
     currentframe = 0
    
     if delete_frames:
@@ -20,21 +20,15 @@ def download_youtube_frames(delete_frames=False, frames_needed=0, link="", frame
                 break
 
     if frames_needed>0:
-        # while True:
-        #     for i in range(frames_skipped):
-        #         frame = stream.read() ### using functions from vidGear module
-        #     if frame is None:
-        #         break
+        while success:
+            currentframe += 1
+            file_name = "frame{}.jpg".format(currentframe)
+            cv2.imwrite(os.path.join(path_and_foldername, file_name), image)
             
-        #     currentframe+=1
-        #     file_name = 'screenshot_{}.jpg'.format(currentframe)
-        #     file_path = path_and_foldername + "/" + file_name
-        #     with open(file_path, 'wb') as f:
-        #         f.write(frame)
-                
-        #     if currentframe==frames_needed:
-        #         break
-
-        # cv2.destroyAllWindows()
-        # stream.stop()
+            for i in range(frames_skipped-1):
+                success,image = vidcap.read()
+            
+            if currentframe==frames_needed:
+                break
+        
         print("\nFRAMES DOWNLOADED: {}\n".format(currentframe))
