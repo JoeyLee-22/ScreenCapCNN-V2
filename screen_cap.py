1aa85c84940178cbd85a28cb8e1a5c0f3d216f16
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2023-02-03 10:43:13 -0800

    version 2.0.0
    saves every screenshot and adds to folder that it classified screenshot as

--- screen_cap.py
+++ screen_cap.py
@@ -5,13 +5,16 @@ import pyautogui
 from PIL import Image
 
 def start_monitor(cnn_instance, seconds=300):    
+    productive_counter = 1
+    unproductive_counter = 1
+    
     while(True):
-        screenshot = pyautogui.screenshot()
-        screenshot.save('screenshots/screenshot.png') # (2100, 3360, 4);
+        screenshot = pyautogui.screenshot() # (2100, 3360, 4)
+        # screenshot.save('screenshots/screenshot.png')
         
-        screenshot = Image.open('screenshots/screenshot.png')
+        # screenshot = Image.open('screenshots/screenshot.png')
         resized_screenshot = screenshot.resize((1280,720))
-        resized_screenshot.save('screenshots/resized_screenshot.png')
+        # resized_screenshot.save('screenshots/resized_screenshot.png')
         
         screenshot = cv2.cvtColor(np.array(resized_screenshot), cv2.COLOR_RGB2BGR)        
         
@@ -20,9 +23,12 @@ def start_monitor(cnn_instance, seconds=300):
         # productive = num close to 0, unproductive = num close to 1
         if (result<=0.5):
             print("\n\n{}: Productive".format(result))
+            resized_screenshot.save('productive_screenshots/productive_screenshot{}.png'.format(productive_counter))
+            productive_counter+=1
         else:
             print("\n\n{}: Unproductive".format(result))
-                 
+            resized_screenshot.save('unproductive_screenshots/unproductive_screenshot{}.png'.format(unproductive_counter)) 
+            unproductive_counter+=1
         for secs in range (seconds):
             time.sleep(0.99)
             print(str(seconds-int(secs)) + "/" + str(seconds), end='\r')
